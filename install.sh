7d8dadfe90ea606974c7c09f89455f2f9d0219ee
Rushan Fayzullin <103266620+shepherl@users.noreply.github.com>
2026-04-18 22:17:39 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,29 @@
-# ID FoxyProxy: gomekmidlodpcbbmibiiaebmhpbhhlca
-EXTENSION_ID="gomekmidlodpcbbmibiiaebmhpbhhlca"
-CRX_URL="https://clients2.google.com/service/update2/crx?response=redirect&os=mac&arch=arm64&os_arch=arm64&nacl_arch=arm64&prod=chromecrx&prodchannel=&prodversion=120.0.6099.129&lang=en-US&acceptformat=crx3&x=id%3D${EXTENSION_ID}%26installsource%3Dondemand%26uc"
+#!/bin/bash
 
-echo "📦 Вызываем окно установки FoxyProxy..."
-open -a "Google Chrome" "$CRX_URL"
+# 1. ID расширения из твоей ссылки
+EXT_ID="gcknhkkoolaabfmlnjonogaaifnjlfnp"
+
+# 2. Формируем актуальную прямую ссылку
+# Мы используем универсальный шаблон Google API
+INSTALL_URL="https://clients2.google.com/service/update2/crx?response=redirect&prodchannel=stable&acceptformat=crx2,crx3&x=id%3D${EXT_ID}%26installsource%3Dondemand%26uc"
+
+# 3. Визуальное уведомление
+osascript -e 'display notification "Подготовка к установке расширения..." with title "KVN Installer" sound name "Glass"'
+
+echo "--- Проверка готовности ---"
+
+# 4. Диалог с пользователем
+ANSWER=$(osascript -e 'display dialog "Установить расширение FoxyProxy в Google Chrome?" buttons {"Отмена", "Установить"} default button "Установить" with title "Настройка прокси"' -e 'button returned of result')
+
+if [ "$ANSWER" = "Установить" ]; then
+    echo "🌐 Открываю прямую ссылку в Chrome..."
+    
+    # Пытаемся открыть в Хроме. Если его нет, открываем в браузере по умолчанию (но установка сработает только в Chromium)
+    open -a "Google Chrome" "$INSTALL_URL" || open "$INSTALL_URL"
+    
+    # 5. Подсказка, что делать дальше
+    sleep 1
+    osascript -e 'display dialog "В Chrome появилось окно подтверждения. Нажми «Добавить расширение» (Add extension), чтобы закончить." buttons {"Ок"} default button "Ок"'
+else
+    echo "❌ Установка отменена пользователем."
+fi
